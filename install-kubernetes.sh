f4079d4dccffe1029fb38deb50d168d513f42a12
gomesuit <gomesuit@gmail.com>
2016-08-23 02:19:24 +0900

    save

--- install-kubernetes.sh
+++ install-kubernetes.sh
@@ -9,3 +9,17 @@ git clone https://github.com/kubernetes/kube-deploy
 cd kube-deploy/docker-multinode
 ./master.sh
 
+
+
+#export MASTER_IP=192.168.33.10
+
+git clone https://github.com/kubernetes/kube-deploy
+cd kube-deploy/docker-multinode
+./worker.sh
+
+
+
+
+#yum install -y kubernetes-client
+
+#### IMPORTANT: This dns manifest is for v1.2.x only! With v1.3, dns is deployed automatically
