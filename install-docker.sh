4ddf0b483ff8d1c84b6c6ff367ccad17b47b84d6
gomesuit <gomesuit@gmail.com>
2017-01-28 12:59:12 +0900

    save

--- install-docker.sh
+++ install-docker.sh
@@ -2,7 +2,9 @@
 
 curl -fsSL https://get.docker.com/ | sh
 
+# yum list --showduplicates | grep docker
 yum downgrade -y docker-engine-1.12.6-1.el7.centos
+yum downgrade -y docker-engine-selinux-1.12.6-1.el7.centos
 
 systemctl enable docker
 systemctl start docker
